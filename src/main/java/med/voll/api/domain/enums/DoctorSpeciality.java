39d84f0b817cb0a0500a3768e6540fe6653e30b6
Levi Eber <gblevieber@gmail.com>
2024-05-23 20:19:44 -0300

    finish 1st course

--- src/main/java/med/voll/api/domain/enums/DoctorSpeciality.java
+++ src/main/java/med/voll/api/domain/enums/DoctorSpeciality.java
@@ -1,20 +1,8 @@
 package med.voll.api.domain.enums;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 public enum DoctorSpeciality {
     ORTHOPEDICS,
     CARDIOLOGY,
     GYNECOLOGY,
-    DERMATOLOGY;
-
-    @JsonCreator
-    public static DoctorSpeciality fromString(String value) {
-        for (DoctorSpeciality speciality : DoctorSpeciality.values()) {
-            if (speciality.name().equalsIgnoreCase(value)) {
-                return speciality;
-            }
-        }
-        throw new IllegalArgumentException("Valor inválido para Especialidade: " + value);
-    }
+    DERMATOLOGY
 }
