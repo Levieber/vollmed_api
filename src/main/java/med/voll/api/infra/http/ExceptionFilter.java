3babfc1030f8234e2c0b3a93550bc18c15bb6528
Levi Eber <gblevieber@gmail.com>
2024-05-24 23:28:42 -0300

    finish 2nd module of 2nd course

---
+++ src/main/java/med/voll/api/infra/http/ExceptionFilter.java
@@ -0,0 +1,30 @@
+package med.voll.api.infra.http;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class ExceptionFilter {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Void> handleNotFoundError() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<BadRequestErrorDto>> handleBadRequestError(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(BadRequestErrorDto::new).toList());
+    }
+
+    public record BadRequestErrorDto(String field, String message) {
+        public BadRequestErrorDto(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
